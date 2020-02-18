7387a4aa7565c51226140539a8136f6c182245ed
mshev <mshev@list.ru>
2020-02-18 20:32:41 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.scss';
+import './index.css';
+
+function App() {
+    return (
+        <div className={styles.root}>
+            <div className={styles.title}>Typescript Examples</div>
+        </div>
+    );
+}
+
+export default App;
